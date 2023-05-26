8013be43832520a0fe951b401a3a5646566f46a6
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2023-05-27 02:21:15 +0600

    up

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,31 +6,31 @@ import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Tooltip } from 'recha
 const Statistics = () => {
 
     const data = [
-        { name: "Assignment 1", value: 47 },
+        { name: "Assignment 1", value: 40 },
         { name: "Assignment 2", value: 0 },
-        { name: "Assignment 3", value: 42 },
-        { name: "Assignment 4", value: 0 },
-        { name: "Assignment 5", value: 42 },
-        { name: "Assignment 6", value: 40 },
+        { name: "Assignment 3", value: 26 },
+        { name: "Assignment 4", value: 15 },
+        { name: "Assignment 5", value: 22 },
+        { name: "Assignment 6", value: 12 },
         { name: "Assignment 7", value: 27 },
-        { name: "Assignment 8", value: 24 }
+        { name: "Assignment 8", value: 16 }
     ]
 
     return (
-        <div className='m-28'>
+        <div className='m-58'>
 
-            <h1 className='bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-bold text-center'>Here is the Seven Assignment Mark Representation</h1>
+            <h1 className='bg-gradient-to-r from-red-500 via-green-500 to-yellow-500 bg-clip-text text-transparent text-5xl font-bold text-center'>8 Ass Mark Representation</h1>
             <hr className='mt-10 mb-10' />
 
             <div className='justify-center'>
-                <PieChart width={1500} height={500}>
+                <PieChart width={1600} height={800}>
                     <Pie
                         dataKey="value"
                         isAnimationActive={false}
                         data={data}
                         
-                        outerRadius={80}
-                        fill='#8884d8'
+                        outerRadius={200}
+                        fill='#9e0404'
                         label
                     ></Pie>
                     <Tooltip></Tooltip>
--- src/components/statisticsChart/Chart.jsx
+++ src/components/statisticsChart/Chart.jsx
@@ -5,11 +5,11 @@ const Chart = () => {
 
     const marks = [
         { name: 'Assignment 1', mark: 78 },
-        { name: 'Assignment 2', mark: 92 },
-        { name: 'Assignment 3', mark: 85 },
-        { name: 'Assignment 4', mark: 88 },
-        { name: 'Assignment 5', mark: 90 },
-        { name: 'Assignment 6', mark: 95 }
+        { name: 'Assignment 2', mark: 80 },
+        { name: 'Assignment 3', mark: 60 },
+        { name: 'Assignment 4', mark: 87 },
+        { name: 'Assignment 5', mark: 70 },
+        { name: 'Assignment 6', mark: 65 }
       ];
 
       const [charts, setCharts] = useState([]);
